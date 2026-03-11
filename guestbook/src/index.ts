895b8e4a3d1af3d9996873fee2b00e806ac4cc51
Vratesh <ghadgevratesh@gmail.com>
2026-03-11 21:32:21 +0530

    ts-prisma-proj

--- guestbook/src/index.ts
+++ guestbook/src/index.ts
@@ -1,2 +1,18 @@
 import { prisma } from './lib/prisma.js'
 
+
+async function addANote(name: string, message: string){
+    const visitingUser = await prisma.signature.create({
+        data:{
+            name,
+            message
+        }
+    })
+    console.log(visitingUser)
+}
+
+await addANote('John Doei', 'Hello, this is a 3rd message from John!')
+
+const users = await prisma.signature.findMany();
+
+console.log("All Guestbook Entries:", users);
